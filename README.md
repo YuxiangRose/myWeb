294d9651d972a9d36b8cf6ce4511d1bc720b7611
YuxiangRose <ddgithub@gmail.com>
2014-08-21 16:14:12 -0700

    try to delete other useless files

--- README.md
+++
@@ -1,4 +0,0 @@
-myWeb
-=====
-
-My first web use github
--- header.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$link = mysql_connect("localhost","root","");
-	if(!$link){
-		echo mysql_errno($link);
-	}
-	mysql_select_db('mydata');
-
-?>
